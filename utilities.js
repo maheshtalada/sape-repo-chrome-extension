d647afb011c1af461d70d492a7008f93cc33944d
shinov <shinov4u@gmail.com>
2014-10-20 15:10:07 +0530

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,4 +1,4 @@
-/**
+/*testing*/
  * API's created with revealing modular pattern
  * Storage , Alaram , Notifications , Repo Sucker & Processed JSON
  * These Wrapper API's will be used for both Background Script & Content Scripts
